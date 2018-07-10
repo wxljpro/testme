7fcb15f52b7d486b286ee1b56082b3c5358fa8d2
frank <wxljpro@163.com>
2018-07-10 11:29:01 +0800

    hello world

--- test.sh
+++ test.sh
@@ -1,1 +1,9 @@
-123
+#!/bin/bash
+while getopts "a:b" args
+do
+    case $args in 
+        a) echo "your val is $OPTARG" ;;
+        b) echo "your val is b" ;;
+        ?) echo "no this is param";exit 1  ;;
+    esac
+done
